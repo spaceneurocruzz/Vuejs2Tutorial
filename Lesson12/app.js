c51030f7b5ab7149d873ebc1faa803da576d9f60
Sandy <speedforcerun@gmail.com>
2019-08-11 22:36:18 +0800

    Add Lesson 12- Looping with v-for

---
+++ Lesson12/app.js
@@ -0,0 +1,19 @@
+new Vue({
+	el:'#vue-app',
+	data:{
+		characters: [
+			'Darth Vader', 'C3-PO', 'R2-D2', 'BB-8'
+		],
+		skywalkers: [
+			{name: 'Anakin', age: 25},
+			{name: 'Luke', age: 30},
+			{name: 'Leia', age: 35},
+		]
+	},
+	methods:{
+		
+	},
+	computed:{
+
+	}
+});
