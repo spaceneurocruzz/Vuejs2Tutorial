4ee4aa71209b423bc341da461328c47fa4d7d4b2
Sandy <speedforcerun@gmail.com>
2019-08-11 18:39:21 +0800

    Add Lesson 9 - Computed Properties

---
+++ Lesson9/app.js
@@ -0,0 +1,21 @@
+new Vue({
+	el:'#vue-app',
+	data:{
+		age:20,
+		a:0,
+		b:0
+	},
+	methods:{
+		
+	},
+	computed:{
+		addToA: function(){
+			console.log('addToA');
+			return this.a + this.age;
+		},
+		addToB: function(){
+			console.log('addToB');
+			return this.b + this.age;
+		},
+	}
+});
