16d759fc6461653f7abd0aae5a0de5d0656e4ac4
Sandy <speedforcerun@gmail.com>
2019-08-11 17:14:30 +0800

    Add Lesson 7- Keyboard Events

---
+++ Lesson7/app.js
@@ -0,0 +1,14 @@
+new Vue({
+	el:'#vue-app',
+	data:{
+	
+	},
+	methods:{
+		logName: function(inc){
+			console.log("You entered your name");
+		},
+		logAge: function(inc){
+			console.log("You entered your age");
+		},
+	}
+});
