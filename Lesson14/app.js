b81f5c7b118935c6f4736dd2a18bdb70ad7d05b8
Sandy <speedforcerun@gmail.com>
2019-08-13 23:43:24 +0800

    Add Lesson 14 - Multiple Vue Instances

---
+++ Lesson14/app.js
@@ -0,0 +1,33 @@
+var one = new Vue({
+	el:'#vue-app-one',
+	data:{
+		title: 'Vue App One'
+	},
+	methods:{
+
+	},
+	computed:{
+		greet: function(){
+			return "Hello from app one!";
+		}
+	}
+});
+
+var two = new Vue({
+	el:'#vue-app-two',
+	data:{
+		title: 'Vue App Two'
+	},
+	methods:{
+		changeTitle: function(){
+			one.title = "Title changed";
+		}
+	},
+	computed:{
+		greet: function(){
+			return "Hello from app two!";
+		}
+	}
+});
+
+two.title = "Changed from outside"
