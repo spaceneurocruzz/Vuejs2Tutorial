9de4fa8ad3db80717596be2e9fcc83b223c0a33c
Hsin-Ju Lin <astroneuro@Hsin-Jus-MacBook-Pro.local>
2019-09-08 19:59:30 +0800

    Add Lesson 17 and 18- Vue CLI and Files and Root Component

---
+++ Lesson17and18/vuejs-playlist/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <!-- <div id="app"> -->
+    <h1>{{title}}</h1>
+    <ninjas></ninjas>
+    <!-- <p>{{greeting()}}</p> -->
+  </div>
+</template>
+
+<script>
+import Ninjas from './Ninjas.vue'
+
+export default {
+  components:{
+    'ninjas': Ninjas
+  },
+  data () {
+    return {
+      //title: 'First Vue.js file!'
+      title: 'Ninja app'
+    }
+  // },
+  // methods:{
+  //   greeting: function(){
+  //     return 'hey';
+  //   }
+  
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Lesson17and18/vuejs-playlist/src/Ninjas.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li v-for="ninja in ninjas" v-bind:key="ninja.id">{{ninja}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: ['flash', 'spidey', 'batman']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
